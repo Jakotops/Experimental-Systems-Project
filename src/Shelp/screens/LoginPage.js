3ed0f961815ebc279ca7c87f412bb29fdbbbfeb9
Jakotops <79404345+Jakotops@users.noreply.github.com>
2024-02-10 14:14:18 +0000

    Added text entries and variables

--- src/Shelp/screens/LoginPage.js
+++ src/Shelp/screens/LoginPage.js
@@ -1,17 +1,30 @@
 // TO DO: Create a login page for the app using the following tutorial: https://youtu.be/ql4J6SpLXZA and firebase authentication
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { TextInput, StyleSheet, Text, View, Button, KeyboardAvoidingView } from 'react-native'
+import React, { useState } from 'react'
 
 const LoginPage = ({navigation}) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <View>
+    <KeyboardAvoidingView>
       <Text>LoginPage</Text>
+      <TextInput 
+        placeholder="Username"
+        value={username}
+        onChangeText={text => setUsername(text)}
+      />
+      <TextInput 
+        placeholder="Password"
+        value={password}
+        onChangeText={text => setPassword(text)}
+        secureTextEntry
+      />
       <Button 
         title="Login"
         onPress={() => navigation.navigate('Authenticated', {screen: 'Camera'})}
       />
       <Button title='Register' onPress={() => navigation.navigate('RegisterPage')} />
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 
--- src/Shelp/screens/RegisterPage.js
+++ src/Shelp/screens/RegisterPage.js
@@ -1,16 +1,42 @@
 // TO DO: Create a registration page for the app using the following tutorial: https://youtu.be/ql4J6SpLXZA and firebase authentication
 // and store the user information in the database
 
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, TextInput, StyleSheet, Text, View, Button } from 'react-native'
+import React, {useState} from 'react'
+
 
 
 const RegisterPage = ({ navigation }) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [ConfirmPassword, setConfirmPassword] = useState('')
+
   return (
-    <View>
+    <KeyboardAvoidingView>
+      <Text>RegisterPage</Text>
+      <TextInput 
+        placeholder="First Name"
+
+      />
+      <TextInput 
+        placeholder="Last Name"
+      />
+      <TextInput 
+        placeholder="Username"
+      />
+      <TextInput 
+        placeholder="Password"
+        secureTextEntry
+      />
+      <TextInput
+        placeholder="Confirm Password"
+        secureTextEntry
+      />
       <Button title='Register' onPress={() => navigation.navigate('LoginPage')} />
       <Button title="Return to Login" onPress={() => navigation.goBack()} />
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 
